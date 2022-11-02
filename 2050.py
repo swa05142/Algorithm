20303d962a7515a3ccfc8df277cc888d426a4261
swa05142 <swa05142@naver.com>
2022-11-02 19:05:08 +0900

    2022/11/2

---
+++ 2050.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi():
+    P, K = map(int, input().split())
+    print(P-K+1)
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
